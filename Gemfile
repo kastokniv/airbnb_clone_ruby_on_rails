32c65897eaa9df0b560c3e59958cce4b82290e18
kastokniv <kastokniv@gmail.com>
2022-12-15 12:51:30 +0900

    Redirect user based on email

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "factory_bot_rails"
+  gem "pry-rails"
   gem "rspec-rails", "~> 6.0.0"
 end
 
@@ -51,4 +53,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
---
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from '@hotwired/stimulus';
+import axios from 'axios';
+
+export default class extends Controller {
+  static targets = ['email', 'submit'];
+
+  connect() {
+    this.submitTarget.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (this.emailTarget.value.length === 0) {
+        // empty
+      } else {
+        axios.get('/api/users_by_email', {
+          params: {
+            email: this.emailTarget.value
+          },
+          headers: {
+            'ACCEPT': 'application/json'
+          }
+        }).then((response) => {
+          Turbo.visit('/users/sign_in');
+        }).catch((response) => {
+          Turbo.visit('/users/sign_up');
+        })
+      }
+    });
+  }
+}
